e316d76d0a20be668c916d8fbad0921b75942bba
Christian Agha <christianagha@gmail.com>
2017-09-19 01:58:17 -0700

    adding problem #3

---
+++ 0x16-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Export data in JSON """
+import json
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    """ Func Def """
+    dict_list = []
+    employee_dict = {}
+
+    url = "https://jsonplaceholder.typicode.com/"
+    tasks = requests.get(url + "todos")
+    tasks = tasks.json()
+    users = requests.get(url + "users/")
+    users = users.json()
+
+    for user in users:
+        for item in tasks:
+            if item["userId"] == user["id"]:
+                json_dict = {}
+                json_dict["username"] = user.get("username")
+                json_dict["task"] = item["title"]
+                json_dict["completed"] = item["completed"]
+                dict_list.append(json_dict)
+        employee_dict[user.get("id")] = dict_list
+        dict_list = []
+    print(json.dumps(employee_dict))
