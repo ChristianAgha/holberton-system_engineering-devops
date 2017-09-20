98e4842594bea3a2a042b1e97d4af417c5135dbb
Christian Agha <christianagha@gmail.com>
2017-09-19 23:11:45 -0700

    added a lint to common-session file

--- 0x17-web_stack_debugging_4/1-user_limit.pp
+++ 0x17-web_stack_debugging_4/1-user_limit.pp
@@ -1,6 +1,7 @@
 # Fixes OS configuration for su holberton
 exec { 'change-os-configuration-for-holberton-user':
   command => 'sed -i \'/nofile 5/d\' /etc/security/limits.conf; \
-  sed -i \'/nofile 4/d\' /etc/security/limits.conf',
+  sed -i \'/nofile 4/d\' /etc/security/limits.conf; \
+  sed -i \'$ asession required\tpam_limits.so\' /etc/pam.d/common-session',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games'
 }
