f84c97e873e83d923920668d038314f61062cf88
Christian Agha <christianagha@gmail.com>
2017-09-19 21:58:27 -0700

    fixed path

--- 0x17-web_stack_debugging_4/1-user_limit.pp
+++ 0x17-web_stack_debugging_4/1-user_limit.pp
@@ -2,5 +2,5 @@
 exec { 'Fix_Holberton_User':
   command => 'sed -i \'s/nofile 5/nofile 500000/g\' /etc/security/limits.conf; \
   sed -i \'s/nofile 4/nofile 500000/g\' /etc/security/limits.conf',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games'
+  path    => '/etc/'
 }
