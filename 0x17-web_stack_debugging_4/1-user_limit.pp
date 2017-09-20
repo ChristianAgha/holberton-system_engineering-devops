716c6bee200df40e94bae963450123148c28a120
Christian Agha <christianagha@gmail.com>
2017-09-19 22:43:31 -0700

    checked title

--- 0x17-web_stack_debugging_4/1-user_limit.pp
+++ 0x17-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,5 @@
 # Fixes OS configuration for su holberton
-exec { 'Fix_Holberton_User':
+exec { 'change-os-configuration-for-holberton-user':
   command => 'sed -i \'/nofile 5/d\' /etc/security/limits.conf; \
   sed -i \'/nofile 4/d\' /etc/security/limits.conf',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games'
